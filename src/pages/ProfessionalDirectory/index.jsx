36510a9ca12dd58db1e46c4b177c946f184184d3
mariyammuad <mariyammuad@gmail.com>
2023-10-19 11:39:08 +0530

    Professional directory & Appointment booking page

--- src/pages/ProfessionalDirectory/index.jsx
+++ src/pages/ProfessionalDirectory/index.jsx
@@ -1,5 +1,131 @@
-const ProfessionalDirectory=()=>
+import './index.css';
+
+const directory=()=>
 {
-    
+    return (
+        <div>
+            <div className = "container">
+                    <div className = "row justify-content-center">
+                        <div className = "col-lg-6">
+                            <h2 className = "text-center heading">GET IN TOUCH WITH A THERAPIST...</h2>
+                            <p className="para">Discover Our Distinguished Network of Dedicated Mental Health Professionals</p>
+                        </div>
+                    </div>
+            </div> 
+            <div class="row row-cols-1 row-cols-md-5 g-4">
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="https://lankasara.com/wp-content/uploads/2023/05/snapshot35.jpg" class="card-img-top" alt="phycho"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Dr. Rumi Ruben</h5>
+                            <p class="card-text">Psychiatrist</p>
+                            <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                            <button class = "btn btn-primary card-button">Chat Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                        <img src="https://nawinna.com/assets/images/doctor/100/187-untitled-1.jpg" class="card-img-top" alt="phycho"></img>
+                        <div class="card-body">
+                            <h5 class="card-title">Card title</h5>
+                            <p class="card-text">Psychiatrist</p>
+                            <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                            <button class = "btn btn-primary card-button">Chat Now</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                            <img src="" class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                            <img src="https://lankasara.com/wp-content/uploads/2023/05/snapshot35.jpg" class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                    </div>
+                </div> 
+                <div class="col">
+                    <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                    </div>
+                </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="https://lankasara.com/wp-content/uploads/2023/05/snapshot35.jpg" class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chat Now</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col">
+                        <div class="card h-100">
+                            <img src="..." class="card-img-top" alt="..."></img>
+                            <div class="card-body">
+                                <h5 class="card-title">Card title</h5>
+                                <p class="card-text">Psychiatrist</p>
+                                <button class = "btn btn-primary card-button">BOOK NOW</button><br/> <br/>
+                                <button class = "btn btn-primary card-button">Chats Now</button>
+                            </div>
+                        </div>
+                    </div>   
+                    </div>
+            </div>            
+           
+    )
 }
-export default ProfessionalDirectory;
+export default directory;

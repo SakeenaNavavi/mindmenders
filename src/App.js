8a1ffce6e254d6d52f33392d4a4586686d86d6aa
PramoW22 <pramodhyamaddekanda@gmail.com>
2023-11-27 16:17:24 +0530

    Edit Checkout

--- src/App.js
+++ src/App.js
@@ -14,6 +14,7 @@ import PrivacyPolicy from './pages/PrivacyPolicy';
 import AddQuizzes from './pages/AddQuizzes';
 import LoginRegister from './pages/LoginRegister';
 import AppointmentBooking from './pages/AppointmentBooking';
+import CheckoutForm from './Components/molecules/CheckoutForm';
 function App() {
   return (
     <div className="App">
@@ -36,6 +37,6 @@ function App() {
       <Footer/>
     </div>
   );
-}
+};
 
 export default App;

d9e149ccf3f4e7d8e69fbfa040407c321674a1fa
SakeenaNavavi <Sakeenanavavi@gmail.com>
2023-12-10 23:54:24 +0530

    Admin dashboard modifications

--- src/Components/molecules/AdminContent/index.jsx
+++ src/Components/molecules/AdminContent/index.jsx
@@ -70,14 +70,7 @@ const AdminContent = () => {
                 {/* <!-- Page Heading --> */}
                 <div class="d-sm-flex align-items-center justify-content-between mb-4">
                     <h1 class="h3 mb-0 text-gray-800">Dashboard</h1>
-                    <a
-                        href="/"
-                        class="
-                  d-none d-sm-inline-block
-                  btn btn-sm btn-primary
-                  shadow-sm
-                "
-                    >
+                    <a href="/" class="d-none d-sm-inline-block btn btn-sm btn-primary shadow-sm">
                         <i class="fas fa-arrow-alt-circle-left fa-sm text-white-50"></i> Visit visitor UI
                     </a>
                 </div>
@@ -90,14 +83,7 @@ const AdminContent = () => {
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div
-                                            class="
-                            text-xs
-                            font-weight-bold
-                            text-primary text-uppercase
-                            mb-1
-                          "
-                                        >
+                                        <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">
                                             Total Users Signed Up
                                         </div>
                                         <div class="h3 mb-0 font-weight-bold text-gray-800">
@@ -119,14 +105,7 @@ const AdminContent = () => {
                             <div class="card-body">
                                 <div class="row no-gutters align-items-center">
                                     <div class="col mr-2">
-                                        <div
-                                            class="
-                            text-xs
-                            font-weight-bold
-                            text-success text-uppercase
-                            mb-1
-                          "
-                                        >
+                                        <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                             Total consultants signed up
                                         </div>
                                         <div class="h3 mb-0 font-weight-bold text-gray-800">
@@ -152,8 +131,7 @@ const AdminContent = () => {
                                         </div>
                                         <div class="row no-gutters align-items-center">
                                             <div class="col-auto">
-                                                <div
-                                                    class="h3 mb-0 mr-3 font-weight-bold text-gray-800">
+                                                <div class="h3 mb-0 mr-3 font-weight-bold text-gray-800">
                                                     50%
                                                 </div>
                                             </div>

120a40612c51fd1a699a743feede2088181caca5
SakeenaNavavi <Sakeenanavavi@gmail.com>
2023-12-12 13:47:27 +0530

    Consultant registration via Admin dashboard

--- src/pages/AdminDashboard/index.jsx
+++ src/pages/AdminDashboard/index.jsx
@@ -1,76 +1,36 @@
 import React from "react";
 import AdminContent from "../../Components/molecules/AdminContent";
 import AdminNavigation from "../../Components/molecules/AdminNavigation";
-
 import './index.css';
 import AdminSidebar from "../../Components/molecules/AdminSidebar";
+import { useState } from 'react';
+import AdminProfessionalDirectory from "../AdminProfessionalDirectory";
 
-const AdminDashboard=()=>{
+const AdminDashboard = () => {
+  const [activeComponent, setActiveComponent] = useState('');
+
   return (
     <>
       <div id="wrapper">
-        <AdminSidebar />
-        {/*  Content Wrapper  */}
-        <div id="content-wrapper" class="d-flex flex-column">
-          {/*  Main Content  */}
-          <div id="content">
-            <AdminNavigation />
+        <AdminSidebar setActiveComponent={setActiveComponent} />
+        <div className="admin-content">
+        </div>
+        <div id="content-wrapper" className="d-flex flex-column">
+      {/*  Main Content  */}
+      <div id="content">
+      <AdminNavigation />
+      {activeComponent === 'professionalDirectory' ? (
+            <AdminProfessionalDirectory />
+          ) : (
+            
             <AdminContent />
-          </div>
-          
-        </div>
-        {/*  End of Content Wrapper  */}
+          )}
       </div>
-      {/* End of Page Wrapper  */}
-
-      {/* Scroll to Top Button */}
-      <a class="scroll-to-top rounded" href="#page-top">
-        <i class="fas fa-angle-up"></i>
+    </div>
+      </div>
+      <a className="scroll-to-top rounded" href="#page-top">
+        <i className="fas fa-angle-up"></i>
       </a>
-
-      {/* Logout Modal */}
-      <div
-        class="modal fade"
-        id="logoutModal"
-        tabindex="-1"
-        role="dialog"
-        aria-labelledby="exampleModalLabel"
-        aria-hidden="true"
-      >
-        <div class="modal-dialog" role="document">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">
-                Ready to Leave?
-              </h5>
-              <button
-                class="close"
-                type="button"
-                data-dismiss="modal"
-                aria-label="Close"
-              >
-                <span aria-hidden="true">×</span>
-              </button>
-            </div>
-            <div class="modal-body">
-              Select "Logout" below if you are ready to end your current
-              session.
-            </div>
-            <div class="modal-footer">
-              <button
-                class="btn btn-secondary"
-                type="button"
-                data-dismiss="modal"
-              >
-                Cancel
-              </button>
-              <a class="btn btn-primary" href="login.html">
-                Logout
-              </a>
-            </div>
-          </div>
-        </div>
-      </div>
     </>
   );
 }

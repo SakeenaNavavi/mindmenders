2f1394c9ac996a1427b5659f269227771ed8a9f0
dayani04 <dayanisandamali0403@gmail.com>
2023-12-13 13:45:29 +0530

    last comimit 2

--- src/pages/HomePage/index1.jsx
+++ src/pages/HomePage/index1.jsx
@@ -1,6 +1,6 @@
 
 import { Container, Row, Col, Button } from 'react-bootstrap';
-import professionals from './professionals.PNG';
+import professionals from './professionals.png';
 import { Link } from 'react-router-dom';
 const Home2 = () => {
     return (
